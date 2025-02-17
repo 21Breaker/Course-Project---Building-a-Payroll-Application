d62eab517f56732cc5a1ae30757eb3bb64670628
21Breaker <jhuttsell2@ivytech.edu>
2025-02-17 16:17:13 -0500

    Update and rename Payroll.java to PayrollApp.java

---
+++ PayrollApp.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class PayrollApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.setVisible(true);
+        });
+    }
+}
