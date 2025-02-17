bb94684c3d1e311eb03ad1a5db61d46715683882
21Breaker <157035437+21Breaker@users.noreply.github.com>
2025-02-17 15:31:08 -0500

    Update and rename MainWindow.java to Employee.java

---
+++ Employee.java
@@ -0,0 +1,22 @@
+public class Employee {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String jobTitle;
+    private String status;
+    private String dateOfBirth;
+    private String gender;
+    private String payType;
+    private String email;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zip;
+    private String picture;
+
+    // Getters and Setters
+    // Constructor
+    // toString method
+}
