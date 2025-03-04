7e4837564c9cc057fb0778b3f32f636129f8299d
21Breaker <jhuttsell2@ivytech.edu>
2025-03-03 20:56:13 -0500

    Create PasswordUtil.java

---
+++ PasswordUtil.java
@@ -0,0 +1,7 @@
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+}
