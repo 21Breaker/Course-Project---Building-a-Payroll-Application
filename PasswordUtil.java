9623bf22e4e17a3bdfca7de9255b7b0bffebf7c2
21Breaker <jhuttsell2@ivytech.edu>
2025-03-06 22:51:59 -0500

    Delete PasswordUtil.java

--- PasswordUtil.java
+++
@@ -1,7 +0,0 @@
-import org.mindrot.jbcrypt.BCrypt;
-
-public class PasswordUtil {
-    public static String hashPassword(String password) {
-        return BCrypt.hashpw(password, BCrypt.gensalt());
-    }
-}
